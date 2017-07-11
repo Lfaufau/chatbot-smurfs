b85610146d7ebca2d17984b005c0a271a6bbc2c2
Arun Louvois <Arun Louvois>
2017-07-11 15:54:42 +0200

    [ADD] add webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,11 +1,17 @@
-var authenticate =
-  app.get('/webhook', function(req, res) {
-    if (req.query['hub.mode'] === 'subscribe' &&
-        req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
-      console.log("Validating webhook");
-      res.status(200).send(req.query['hub.challenge']);
+var chatservices = require('~/server/chatservice');
+var authenticate = chatservices.authenticate;
+var express = require('express');
+var router = express.Router();
+
+/* GET hello world page. */
+router.get('/', function(req, res, next) {
+  if (authenticate(req))
+    res.status(200).send(req.query['hub.challenge']);
     } else {
-      console.error("Failed validation. Make sure the validation tokens match.");
-      res.sendStatus(403);
-      }
-   });
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+    }
+});
+
+module.exports = router;
+
