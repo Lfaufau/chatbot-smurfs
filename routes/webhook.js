37538067d2043cf86e4c0aa0889b3253e47c7367
Arun Louvois <Arun Louvois>
2017-07-11 15:37:20 +0200

    [ADD] add webhook.js

---
+++ routes/webhook.js
@@ -0,0 +1,11 @@
+var authenticate =
+  app.get('/webhook', function(req, res) {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
+      console.log("Validating webhook");
+      res.status(200).send(req.query['hub.challenge']);
+    } else {
+      console.error("Failed validation. Make sure the validation tokens match.");
+      res.sendStatus(403);
+      }
+   });
