6faad5bc8383690b7cffa124c5db6fbe3473c5ca
Arun Louvois <Arun Louvois>
2017-07-11 16:12:03 +0200

    [ADD] correcting webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,4 +1,4 @@
-var chatservices = require('~/server/chatservice');
+var chatservices = require('../server/chatservice');
 var authenticate = chatservices.authenticate;
 var express = require('express');
 var router = express.Router();
