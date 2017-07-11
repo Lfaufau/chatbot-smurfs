8c324f6e456ec72fb4426fb92b1a7933b6dec2bb
Lucas Fauchille <fauchi_l@epita.fr>
2017-07-11 16:34:07 +0200

    [FIX] connerie…

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', homepage);
+app.use('/webhook', webhook);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
